fa860712caafd89fda3e05ef3dc9e4fa2a03c31c
Peter Lange <lanoitakude@gmail.com>
2017-09-06 09:05:04 -0700

    Removed dependency on BUTTERKNIFE.

--- app/src/main/java/com/personal/peter/easydice/MainActivity.java
+++ app/src/main/java/com/personal/peter/easydice/MainActivity.java
@@ -10,12 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.Set;
 
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import static android.view.View.*;
@@ -30,58 +28,42 @@ public class MainActivity extends AppCompatActivity {
     private HashMap<String, EditText> mTextMap;
     private AnimatorUtil animatorUtil;
 
-    String resultsString;
-    @BindView(R.id.resultTextView)
     TextView mResultTextView;
-    @BindView(R.id.totalTextView)
     TextView mTotalTextView;
-
-    @BindView(R.id.d4Button)
-    Button mD4Button;
-    @BindView(R.id.d6Button)
-    Button mD6Button;
-    @BindView(R.id.d8Button)
-    Button mD8Button;
-    @BindView(R.id.d10Button)
-    Button mD10Button;
-    @BindView(R.id.d12Button)
-    Button mD12Button;
-    @BindView(R.id.d20Button)
-    Button mD20Button;
-
-    @BindView(R.id.d4EditText)
-    EditText mD4EditText;
-    @BindView(R.id.d6EditText)
-    EditText mD6EditText;
-    @BindView(R.id.d8EditText)
-    EditText mD8EditText;
-    @BindView(R.id.d10EditText)
-    EditText mD10EditText;
-    @BindView(R.id.d12EditText)
-    EditText mD12EditText;
-    @BindView(R.id.d20EditText)
-    EditText mD20EditText;
-
-    @BindView(R.id.rollArrayLabel)
     TextView mRollArrayLabel;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
         animatorUtil = new AnimatorUtil(this);
         final DiceUtil diceUtil = new DiceUtil(this, mTotalTextView, mResultTextView);
 
-        //initializes the buttons and edit text. Add more dice here if desired.
         mTextMap = new HashMap<>();
-        initializer(mD4Button, mD4EditText, 4);
-        initializer(mD6Button, mD6EditText, 6);
-        initializer(mD8Button, mD8EditText, 8);
-        initializer(mD10Button, mD10EditText, 10);
-        initializer(mD12Button, mD12EditText, 12);
-        initializer(mD20Button, mD20EditText, 20);
+        mTotalTextView = (TextView) findViewById(R.id.totalTextView);
+        mResultTextView = (TextView) findViewById(R.id.resultTextView);
+        mRollArrayLabel = (TextView) findViewById(R.id.rollArrayLabel);
+
+        //initializes the buttons and edit text. Add more dice here if desired.
+        Button mD4Button = (Button) findViewById(R.id.d4Button);
+        EditText mD4EditText = (EditText) findViewById(R.id.d4EditText);
+        Button mD6Button = (Button) findViewById(R.id.d6Button);
+        EditText mD6EditText = (EditText) findViewById(R.id.d6EditText);
+        Button mD8Button = (Button) findViewById(R.id.d8Button);
+        EditText mD8EditText = (EditText) findViewById(R.id.d8EditText);
+        Button mD10Button = (Button) findViewById(R.id.d10Button);
+        EditText mD10EditText = (EditText) findViewById(R.id.d10EditText);
+        Button mD12Button = (Button) findViewById(R.id.d12Button);
+        EditText mD12EditText = (EditText) findViewById(R.id.d12EditText);
+        Button mD20Button = (Button) findViewById(R.id.d20Button);
+        EditText mD20EditText = (EditText) findViewById(R.id.d20EditText);
+
+        buttonInitializer(mD4Button, mD4EditText, 4);
+        buttonInitializer(mD6Button, mD6EditText, 6);
+        buttonInitializer(mD8Button, mD8EditText, 8);
+        buttonInitializer(mD10Button, mD10EditText, 10);
+        buttonInitializer(mD12Button, mD12EditText, 12);
+        buttonInitializer(mD20Button, mD20EditText, 20);
 
         //Handle saved dice results and inputs, loads them, etc
         mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
@@ -131,8 +113,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void initializer(final Button button, final EditText editText, final int dieSize){
+    public void buttonInitializer(final Button button, final EditText editText, final int dieSize){
+
         mTextMap.put("KEY_D"+dieSize, editText);
+
         button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
